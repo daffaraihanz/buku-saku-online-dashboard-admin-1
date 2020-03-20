8c3dd1de37bbbecc7552d792a49648be27240ec3
rafiudd <sirin6867@gmail.com>
2020-03-21 00:31:23 +0700

    implement api create kelas

--- src/views/AddKelas.js
+++ src/views/AddKelas.js
@@ -1,9 +1,55 @@
 import React, {Component} from 'react';
 import { Container, Row, Col, Card, CardHeader, CardBody,Button,ListGroup,ListGroupItem,Form,FormInput } from "shards-react";
 import PageTitle from "../components/common/PageTitle";
+import axios from 'axios'
 import './style/Overlay.css'
 
 class AddSiswa extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            kelas: '',
+            wali_kelas: '',
+            jurusan: ''
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.addKelas = this.addKelas.bind(this);
+    }
+
+    handleChange = (event)=> {
+        this.setState({[event.target.name]: event.target.value });
+        console.log(event)
+    }
+
+    async addKelas() {
+        try {
+            const baseUrl = 'http://3.91.42.49'
+            const token = localStorage.getItem('token')
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            let model = {
+                kelas: this.state.kelas,
+                wali_kelas: this.state.wali_kelas,
+                jurusan: this.state.jurusan
+            }
+            console.log(model)
+            const query = await axios.post(`${baseUrl}/api/kelas/create`,model, config)
+            console.log(query)
+            if(query.data.code <= 200){
+                alert('Sukses Tambah Kelas')
+                document.location.href = "/data-siswa"                  
+            } else {
+              alert('ERROR Ada yang salah')
+            }
+          } catch (error) {
+            alert('Ada yang salah')
+        }
+    }
 
     toDaftarKelas(){
         document.location.href = "/daftar-kelas"
@@ -28,7 +74,7 @@ class AddSiswa extends Component{
                                     <Col lg="6" md="8" className="form-group">
                                     <label htmlFor="feFirstName">Kelas</label>
                                     <FormInput
-                                        name="name"
+                                        name="kelas"
                                         placeholder="contoh: XII RPL 2"
                                         // value={this.state.name}
                                         onChange={this.handleChange}
@@ -42,7 +88,7 @@ class AddSiswa extends Component{
                                     <FormInput
                                         // type="email"
                                         // id="feEmail"
-                                        // name="email"
+                                        name="wali_kelas"
                                         placeholder="Masukan Wali Kelas"
                                         // value={this.state.email}
                                         onChange={this.handleChange}
@@ -52,7 +98,23 @@ class AddSiswa extends Component{
                                     {/* Password */}
                                     
                                 </Row>
-                                <Button  theme="accent">Tambah Kelas</Button>
+                                <Row form>
+                                    {/* NAMA */}
+                                    <Col lg="6" md="8" className="form-group">
+                                    <label htmlFor="feEmail">Jurusan</label>
+                                    <FormInput
+                                        // type="email"
+                                        // id="feEmail"
+                                        name="jurusan"
+                                        placeholder="Masukan Jurusan"
+                                        // value={this.state.email}
+                                        onChange={this.handleChange}
+                                    />
+                                    </Col>
+                                    {/* Password */}
+                                    
+                                </Row>
+                                <Button  theme="accent" onClick={this.addKelas}>Tambah Kelas</Button>
                             </Form>
                         </Col>
                         </Row>
