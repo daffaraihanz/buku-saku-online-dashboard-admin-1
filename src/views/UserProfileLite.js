a466c746bdb892b3b6da1361f56da80be4836ed8
Valerie <daffaraihanzaki@gmail.com>
2020-03-22 22:27:19 +0700

    add profil

--- src/views/UserProfileLite.js
+++ src/views/UserProfileLite.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "shards-react";
+import { Container, Row, Col,Card,CardBody } from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
 import UserDetails from "../components/user-profile-lite/UserDetails";
@@ -8,15 +8,23 @@ import UserAccountDetails from "../components/user-profile-lite/UserAccountDetai
 const UserProfileLite = () => (
   <Container fluid className="main-content-container px-4">
     <Row noGutters className="page-header py-4">
-      <PageTitle title="Lala" subtitle="Overview" md="12" className="ml-sm-auto mr-sm-auto" />
+      <PageTitle title="Profil" subtitle="Buku saku online" md="12" className="ml-sm-auto mr-sm-auto" />
     </Row>
     <Row>
       <Col lg="4">
-        <UserDetails />
+        <Card>
+          <CardBody>
+            <div className="text-center">
+                <img className="rounded-circle" src="https://placeimg.com/150/150/any"></img>
+                <h4 className="mt-3 mb-0">Lala Rara</h4>
+                <p className="m-0" style={{fontWeight: 300}}>lala@gmail.com</p>
+            </div>
+          </CardBody>
+        </Card>
       </Col>
-      <Col lg="8">
+      {/* <Col lg="8">
         <UserAccountDetails />
-      </Col>
+      </Col> */}
     </Row>
   </Container>
 );
