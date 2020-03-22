e41e6e0f8b94d600fbdfeccaff8b00ff21e2da32
rafiudd <sirin6867@gmail.com>
2020-03-22 23:27:40 +0700

    implement: api get detail point

--- src/views/DetailKategori.js
+++ src/views/DetailKategori.js
@@ -9,12 +9,20 @@ class DetailKategori extends Component {
         this.state = {
             modalHapus: true,
             modalEdit: true,
-            data: []
+            data: [],
+            idHapus: '',
+            kode: '',
+            jenis_pelanggaran: '',
+            point: '',
+            kategori: ''
         }
         this.hapus = this.alertHapus.bind(this);
         this.alertHapus = this.alertHapus.bind(this);
         this.alertEdit = this.alertEdit.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
+        this.deleteKategori = this.deleteKategori.bind(this);
+        this.updateKategori = this.updateKategori.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     alertHapus(id){
@@ -33,7 +41,7 @@ class DetailKategori extends Component {
         })
       }
     
-      alertEdit (){
+      alertEdit (id){
         if( this.state.modalEdit === true){
           this.setState({
             modalEdit: false
@@ -43,6 +51,9 @@ class DetailKategori extends Component {
               modalEdit: true
             })
         }
+        this.setState({
+            idHapus: id
+          })
       }
 
       hapus() {
@@ -69,15 +80,14 @@ class DetailKategori extends Component {
                   Authorization: `Bearer ${localStorage.getItem('token')}`
               }
           }
-          let id = this.props.location.state
-          console.log(id,['id'])
+          let kategori = this.props.location.state
+          console.log(kategori,['id'])
           
           await axios.get(
-              `${baseUrl}/api/peraturan?id=${id}`,
+              `${baseUrl}/api/point/kategori?kategori=${kategori}`,
               config
           ).then(response => {
-              console.log(response,['ANJG'])
-              this.setState({ data: response.data.data.pasal })
+              this.setState({ data: response.data.data })
           })
         } catch (error) {
             alert(error)
@@ -87,20 +97,67 @@ class DetailKategori extends Component {
     toAddKategori(){
         document.location.href = "/add-detail-kategori"
     }
+
+    async updateKategori() {
+        try {
+          const baseUrl = 'http://3.91.42.49'
+          const token = localStorage.getItem('token')
+          const config = {
+              headers: {
+                  Authorization: `Bearer ${token}`
+              }
+          }
+          let data = {
+              kode: this.state.kode,
+              jenis_pelanggaran: this.state.jenis_pelanggaran,
+              point: this.state.point,
+              kategori: this.state.kategori
+          }
+          console.log(data)
+          const update = await axios.put(`${baseUrl}/api/point/edit?id=${this.state.idHapus}`,data, config)
+          console.log(update)
+          if(update.data.code <= 200){
+              alert('Sukses Update Kategori Point')
+              document.location.href = "/daftar-point"                  
+          } else {
+            alert('ERROR Ada yang salah')
+          }
+        } catch (error) {
+          alert('Ada yang salah')
+        }
+    } 
+
+    handleChange = (event)=> {
+        this.setState({[event.target.name]: event.target.value });
+        console.log(event.target.value)
+    }
+
+    async deleteKategori() {
+      try {
+          const token = localStorage.getItem('token')
+          const config = {
+              headers: {
+                  Authorization: `Bearer ${token}`
+              }
+          }
+          const baseUrl = 'http://3.91.42.49'
+  
+          await axios.delete(`${baseUrl}/api/point/remove?id=${this.state.idHapus}`, config).then(response => {
+              if(response.data.code === 200) {
+                  alert("Berhasil Hapus Point Kategori");
+                  document.location.href = "/daftar-point"                  
+              }
+          })
+      } catch (error) {
+          alert(error)
+      }
+    }
+
     render(){
         return(
             <Container fluid className="main-content-container px-4">
                  <Row noGutters className="page-header py-4">
                     <PageTitle sm="4" title="Daftar Point" subtitle="Buku Saku Online" className="text-sm-left" />
-                </Row>
-                <Row className="mb-2">
-                    <Col>
-                        <div className="d-flex justify-content-end">
-                            <Button theme="primary"  onClick={this.toAddKategori} className="mb-2">
-                                <i className="material-icons mr-1">add</i> Tambah Detail Kategori
-                            </Button>
-                        </div>
-                    </Col>
                 </Row>
                 <Row className="px-3">
                     <table className="table mb-0">
@@ -113,23 +170,37 @@ class DetailKategori extends Component {
                             Detail Kategori
                         </th>
                         <th scope="col" className="border-0 text-white">
+                            Kode
+                        </th>
+                        <th scope="col" className="border-0 text-white">
+                            Point
+                        </th>
+                        <th scope="col" className="border-0 text-white">
                             Action
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Delaaa</td>
-                        <td>
-                            <Button onClick={this.alertEdit} theme="primary" className="mb-2 mr-2">
-                                Edit
-                            </Button>
-                            <Button onClick={this.alertHapus}theme="danger" className="mb-2">
-                                Delete
-                            </Button>
-                        </td>
-                    </tr>
+                    {this.state.data.map((item,key) => {
+                        return(
+                            <>
+                            <tr>
+                                <td>{key + 1}</td>
+                                <td>{item.jenis_pelanggaran}</td>
+                                <td>{item.kode}</td>
+                                <td>{item.point}</td>
+                                <td>
+                                    <Button onClick={() => this.alertEdit(item._id)} theme="primary" className="mb-2 mr-2">
+                                        Edit
+                                    </Button>
+                                    <Button onClick={() => this.alertHapus(item._id)}theme="danger" className="mb-2">
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                            </>
+                        )
+                    })}
                 </tbody>
                 </table>
             </Row>
@@ -139,7 +210,7 @@ class DetailKategori extends Component {
                     <Card>
                         <CardBody className="text-center">
                     <p>Apakah Anda yakin ingin menghapus data?</p>
-                    <Button className="btn btn-danger mr-2" onClick={this.hapus}>Yes</Button>
+                    <Button className="btn btn-danger mr-2" onClick={this.deleteKategori}>Yes</Button>
                     <Button className="btn btn-primary" onClick={this.alertHapus}>No</Button>
                     </CardBody>
                     </Card>
@@ -153,11 +224,20 @@ class DetailKategori extends Component {
                     <Card>
                     <CardBody>
                     <FormGroup>
-                        <label htmlFor="judul">Masukkan Detail Kategori Baru</label>
-                        <textarea style={{width: '100%'}} className="mb-3" id="judul" placeholder="Detail Kategori Baru"></textarea>
+                        <label htmlFor="kode">Kode</label>
+                        <input onChange={this.handleChange} style={{width: '100%'}} className="mb-3" id="kode" name="kode" placeholder="Masukan Kode"/>
+
+                        <label htmlFor="jenis_pelanggaran">Jenis Pelanggaran</label>
+                        <input onChange={this.handleChange} style={{width: '100%'}} className="mb-3" id="jenis_pelanggaran" name="jenis_pelanggaran" placeholder="Masukan Jenis Pelanggaran"/>
+
+                        <label htmlFor="point">Point</label>
+                        <input onChange={this.handleChange} style={{width: '100%'}} className="mb-3" id="point" name="point" placeholder="Masukan Point"/>
+
+                        <label htmlFor="kategori">Kategori</label>
+                        <input onChange={this.handleChange} style={{width: '100%'}} className="mb-3" id="kategori" name="kategori" placeholder="Masukan kategori"/>
                     </FormGroup>
                     <div className="mt-2">
-                        <Button className="btn btn-primary mr-2" onClick={this.alertEdit}>Simpan</Button>
+                        <Button className="btn btn-primary mr-2" onClick={this.updateKategori}>Simpan</Button>
                         <Button className="btn btn-danger" onClick={this.alertEdit}>Batal</Button>
                     </div>
                 </CardBody>
